6e7caaa9d0660f1519fda1b25b64f78544a480de
kendy-karakawa <kendy.karakawa@gmail.com>
2022-12-05 22:04:16 -0300

    layout do componente listafilme ok

---
+++ src/components/ListaFilme.js
@@ -0,0 +1,82 @@
+import styled from "styled-components";
+
+export default function ListaFilmes() {
+  return (
+    <ScreenContainer>
+      <Header>CINEFLEX</Header>
+      <Title>Selecione o filme</Title>
+      <Poster><img src="" alt="" /></Poster>
+      <Poster><img src="" alt="" /></Poster>
+      <Poster><img src="" alt="" /></Poster>
+      <Poster><img src="" alt="" /></Poster>
+      <Poster><img src="" alt="" /></Poster>
+      <Poster><img src="" alt="" /></Poster>
+    </ScreenContainer>
+  );
+}
+
+const ScreenContainer = styled.div`
+  width:100%;
+  min-width: 400px;
+  min-height: 100vh;
+  background-color: #e5e5e5;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: flex-start;
+  justify-content: center;
+  margin: 0px;
+  padding: 0px;
+  padding-top: 50px;
+`;
+
+const Title = styled.p`
+  height: 110px;
+  width:100%;
+  min-width: 400px;
+  padding-top: 50px;
+  padding-bottom: 20px;
+  display: flex;
+  justify-content: center;
+  font-family: "Roboto";
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 28px;
+  letter-spacing: 0.04em;
+  color: #293845;
+`;
+
+const Header = styled.header`
+  width:100%;
+  min-width: 400px;
+  min-height: 50px;
+  background-color: #c3cfd9;
+  position: fixed;
+  top: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-family: "Roboto";
+  font-weight: 400;
+  font-size: 34px;
+  color: #e8833a;
+  padding: 10px;
+  z-index: 1;
+`;
+
+const Poster = styled.div`
+  width: 145px;
+  height: 209px;
+  background: #ffffff;
+  box-shadow: 0px 2px 4px 2px rgba(0, 0, 0, 0.1);
+  border-radius: 3px;
+  padding: 8px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 20px;
+  img {
+    width: 129px;
+    height: 193px;
+    
+  }
+`;
